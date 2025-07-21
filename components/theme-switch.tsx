ea8bfdfd37c173fb33f02fc5636bda4b823ad605
Dany <danysalgueiro80@gmail.com>
2025-07-22 01:48:51 +0200

    fix: make theme switch instant by removing delay from toggle logic

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -11,9 +11,7 @@ export default function ThemeSwitch() {
 
   const handleClick = () => {
     setRotation(rotation + 360);
-    setTimeout(() => {
-      toggleTheme();
-    }, 400);
+    toggleTheme(); // Change theme instantly
   };
 
   return (
