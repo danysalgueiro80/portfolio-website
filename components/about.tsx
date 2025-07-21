84b19ddbada2e6dcd5d080f5f28e17e9b3e85d14
Dany <danysalgueiro80@gmail.com>
2025-07-22 01:29:53 +0200

    style: revert About Me highlights to bold instead of color

--- components/about.tsx
+++ components/about.tsx
@@ -20,10 +20,10 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
       I am a portuguese tech enthusiast, currently based in{" "}
-      <span className="text-[#2D9EDB]">Madrid</span>, with a strong passion <span className="text-[#2D9EDB]">Data</span>, <span className="text-[#2D9EDB]">CRM</span>, and the <span className="text-[#2D9EDB]">Salesforce ecosystem</span>.
+      <b>Madrid</b>, with a strong passion <b>Data</b>, <b>CRM</b>, and the <b>Salesforce ecosystem</b>.
       </p>
       <p className="mb-3">
-      My journey into technology started with a background in marketing, where I quickly realized that <span className="text-[#2D9EDB]">CRM</span> was the perfect bridge between business and tech. Since then, I’ve been diving deeper into the world of <span className="text-[#2D9EDB]">data</span>, marketing automation and problem-solving. I love the challenge of untangling complex issues, learning new tools, and constantly pushing myself to improve, and this website is just one of the many projects where I get to explore new technologies and skills.
+      My journey into technology started with a background in marketing, where I quickly realized that <b>CRM</b> was the perfect bridge between business and tech. Since then, I’ve been diving deeper into the world of <b>data</b>, marketing automation and problem-solving. I love the challenge of untangling complex issues, learning new tools, and constantly pushing myself to improve, and this website is just one of the many projects where I get to explore new technologies and skills.
       </p>
       <p className="mb-3">
       When it’s time to relax, you’ll probably find me playing video games, reading, or just enjoying some downtime with family and friends.
