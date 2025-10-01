981244976545374349230790d1d98bd42bfb45d1
Dany <danysalgueiro80@gmail.com>
2025-10-01 22:25:47 +0200

    Update CV download link to CVDanySalgueiro2025.pdf

--- components/intro.tsx
+++ components/intro.tsx
@@ -200,7 +200,7 @@ export default function Intro() {
                 <Link href="#contact" className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition cursor-pointer border border-black/10">
                 Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
             </Link>
-            <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10" href="/CV_DanySalgueiro_2024.pdf" download>Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition"/> </a>
+            <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10" href="/CVDanySalgueiro2025.pdf" download>Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition"/> </a>
             
             <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10 dark:bg-white/10 dark:text-white/60" href="https://www.linkedin.com/in/danysalgueiro/" target="_blank"><BsLinkedin /></a>
             </motion.div>
