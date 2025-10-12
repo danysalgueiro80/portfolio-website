55e04ef00fdf99ecf1f83d6068a7152654664c85
Dany <danysalgueiro80@gmail.com>
2025-10-12 23:47:22 +0200

    Widen navigation bar: increase width for better active section spacing

--- components/header.tsx
+++ components/header.tsx
@@ -14,13 +14,13 @@ export default function Header() {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 h-[4.5rem] max-w-[1200px] w-full -translate-x-1/2 rounded-none border border-white/40 bg-white/90 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:rounded-full sm:w-[600px] dark:bg-gray-950/90 dark:border-black/40"
+        className="fixed top-0 left-1/2 h-[4.5rem] max-w-[1200px] w-full -translate-x-1/2 rounded-none border border-white/40 bg-white/90 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:rounded-full sm:w-[650px] dark:bg-gray-950/90 dark:border-black/40"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+        <ul className="flex w-[24rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
